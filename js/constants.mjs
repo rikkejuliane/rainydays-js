c867ccd40c5d7288f87346c76152357282bcdb6d
rikkejuliane <rikand01864@stud.noroff.no>
2024-04-28 12:32:43 +0200

    first commit

--- js/constants.mjs
+++ js/constants.mjs
@@ -1,3 +1,3 @@
 const API_BASE_URL = 'https://api.noroff.dev/api/v1/rainy-days';
 
-export const API_POSTS_URL = `${API_BASE_URL}`;
+export const API_JACKETS_URL = `${API_BASE_URL}`;
--- js/shop.mjs
+++ js/shop.mjs
@@ -1,11 +1,11 @@
-import { API_POSTS_URL } from "./constants.mjs";
+import { API_JACKETS_URL } from "./constants.mjs";
 import { fetchData } from "./fetchData.mjs";
 import { createJacketsInHtml } from "./createJacketsInHtml.mjs";
 
 
 
 async function shop() {
-    const jackets = await fetchData(API_POSTS_URL);
+    const jackets = await fetchData(API_JACKETS_URL);
     console.log(jackets)
     createJacketsInHtml(jackets);
 }
